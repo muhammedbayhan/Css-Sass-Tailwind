64ed319d38737c630050dc10496fa98636803d87
muhammedbayhan <muhammedbayhan72@gmail.com>
2024-04-22 16:31:44 +0300

    removed carrent

--- Tailwind/comp/src/App.jsx
+++ Tailwind/comp/src/App.jsx
@@ -1,18 +1,9 @@
 
-import car from './assets/car.png';
-import carlook from './assets/carlook.png';
-
 function App() {
 
   return (
 
-    <div className="bg-lightwhite h-screen flex font-jakarta ">
-
-    
-
-
-    </div>
-
+  <div></div>
   )
 }
 
